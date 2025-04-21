8337dd8fa1de99b891b43403be0e74f0658bdc8c
coderceder61 <codercoder61@gmail.com>
2025-04-21 17:40:37 +0100

    commit

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
 
-const fetch = require('node-fetch')
-
-const cors = require('cors')
-
-app.use(cors())
+const app = express();
+app.use(cors());
 app.use(express.json());
 
-app.post('/fetch-url',async (req, res) => {
-    const { url } = req.body;
-    try {
-        const response = await fetch(url);
-        const data = await response.json(); // use response.json() if API returns JSON
-        res.json({ message: 'Data fetched successfully', content: data });
-      } catch (err) {
-        res.status(500).json({ message: 'Error fetching URL', error: err.message });
-      }
-  });
-app.listen(3000,()=>{
-    console.log('listening on port 3000')
-})
+app.post('/fetch-url', async (req, res) => {
+  const { url } = req.body;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.json({content: data });
+  } catch (err) {
+    res.status(500).json({ message: 'Error fetching URL', error: err.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
